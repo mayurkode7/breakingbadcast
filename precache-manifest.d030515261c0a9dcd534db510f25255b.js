232dda0dd5a6baf62623a3a12c75b741ae9ac6a0
Mayur Kode <mayurkode7@gmail.com>
2020-07-07 13:02:31 +0530

    Updates

---
+++ precache-manifest.d030515261c0a9dcd534db510f25255b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f70cbc55d7f6162e887a7dc9e547213",
+    "url": "/breakingbadcast/index.html"
+  },
+  {
+    "revision": "0a8422228d72afc95352",
+    "url": "/breakingbadcast/static/css/main.574023a0.chunk.css"
+  },
+  {
+    "revision": "5f490e11972b2fd6c7e3",
+    "url": "/breakingbadcast/static/js/2.973729d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbadcast/static/js/2.973729d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8422228d72afc95352",
+    "url": "/breakingbadcast/static/js/main.d81662ae.chunk.js"
+  },
+  {
+    "revision": "5f2b443c600911267f6d",
+    "url": "/breakingbadcast/static/js/runtime-main.77a4f56c.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbadcast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbadcast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakingbadcast/static/media/spinner.235b9b3e.gif"
+  }
+]);
